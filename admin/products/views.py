f83dbd2c8c4d6fd32b81185eb2464fcf5928bcc2
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-08 17:10:24 -0700

    complete rough admin django app

--- admin/products/views.py
+++ admin/products/views.py
@@ -18,7 +18,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        publish('product_created', serializer.data)
+        # publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -31,13 +31,13 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        publish('product_updated', serializer.data)
+        # publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
-        publish('product_deleted', pk)
+        # publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
