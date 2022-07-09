2819c1e2686d12b3e8a73fb2385fe6ecb3144d8b
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-08 17:07:21 -0700

    initialzie api products route

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import ProductViewSet
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('products.urls'))
+    path('products', ProductViewSet.as_view({
+      'get': 'list',
+      'post': 'create'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+      'get': 'retrieve',
+      'put': 'update',
+      'delete': 'destroy'
+    })),
 ]
